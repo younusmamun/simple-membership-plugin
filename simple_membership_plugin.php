89c3c7e86be9a1594f633432ecb2657d4ce14a10
younus mamun <younusmamun@gmail.com>
2022-03-28 15:40:40 +0600

    membership system add

--- simple_membership_plugin.php
+++ simple_membership_plugin.php
@@ -40,26 +40,35 @@ function cmb2_sample_metaboxes() {
 
 	
 
+	$terms = get_terms( array(
+		'taxonomy' => 'departments',
+		'hide_empty' => false,
+	) );
+	$term_id = [];
+	foreach($terms as $term){
+		$term_id[] =  $term->term_id;
+	}
 
-	$all_users_data = get_users();
-	$users_id = [];
-	foreach($all_users_data as $user){	
-		$users_id[] =  $user->ID;
-	};
+
+	$term_name = [];
+	foreach($terms as $term){
+		$term_name[] =  $term->name;
+	}
 	
-	$users_name = [];
-	foreach($all_users_data as $username){	
-		$users_name[] =  $username->display_name;
-	};
-	$users_id_and_name = array_combine($users_id, $users_name);
+
+	$terms_id_and_name = array_combine($term_id, $term_name);
+	
 
 	$cmb->add_field( array(
-		'name'    => 'Select User',
-		'id'      => 'selected_user',
+		'name'    => 'Select membershhip',
+		'id'      => 'selected_user12',
 		'desc'    => 'Select user. Drag to reorder.',
 		'type'    => 'pw_multiselect',
-		'options' => $users_id_and_name
+		'options' => $terms_id_and_name
 	) );
+
+
+
 }	
 
 
@@ -98,9 +107,73 @@ function add_read_more_button($content){
 	}
 }
 
-add_action('the_content','add_read_more_button');
+//add_action('the_content','add_read_more_button');
 
 
+
+
+
+function add_read_more_button2($content){
+
+
+	global $post;
+	$premium_post = get_post_meta($post->ID, 'premium', true);
+	$selected_memberships_term_id = get_post_meta($post->ID, 'selected_user12', true);
+	var_dump($selected_memberships_term_id);
+	// 9->5,6->.1
+
+	
+	//$selected_memberships_term_id = array("9","6");
+	$all_selected_user_in_post = [];
+		foreach($selected_memberships_term_id as $term_id){
+		
+		  $term_vals = get_term_meta($term_id);
+	
+		 // $all_selected_user_in_post = [];
+		  foreach($term_vals as $key=>$val){
+	
+			  $selected_user_in_post = $val[0];
+			  var_dump($selected_user_in_post);
+			  $final_selected_user_in_post = unserialize($selected_user_in_post);
+			  $all_selected_user_in_post = array_merge($all_selected_user_in_post, $final_selected_user_in_post);
+			  var_dump($all_selected_user_in_post);
+	
+		  }
+		}
+	
+
+	if($premium_post ){
+
+		if(is_user_logged_in()){
+			$logedin_user_value= wp_get_current_user();
+			
+			$current_logedin_user_id = strval($logedin_user_value->ID);
+			var_dump($current_logedin_user_id);
+			
+
+			if(in_array($current_logedin_user_id, $all_selected_user_in_post)){
+				echo 'User ID: ' . get_current_user_id();
+				$content .= 'premium content and user loged in';
+				return $content;
+			}else{
+				?>
+				<p>You are loged in but not permitted for this content</p>
+				<?php
+			}	
+		}else{
+			
+			?>
+				<p>This content is premium. If you want to read please login</p>
+				<a href="<?php echo wp_login_url( get_permalink() ); ?>" title="Login"> Login</a>
+			<?php
+		}
+		
+	}else{
+		return $content;
+	}
+}
+
+add_action('the_content','add_read_more_button2');
 
 
 
@@ -109,26 +182,26 @@ add_action('the_content','add_read_more_button');
 function custom_taxonomy() {
 
 	$labels = array(
-	  'name'                       => _x( 'Departments', 'Departments Name', 'text_domain' ),
-	  'singular_name'              => _x( 'Department', 'Department Name', 'text_domain' ),
-	  'menu_name'                  => __( 'Departments', 'text_domain' ),
-	  'all_items'                  => __( 'All Departments', 'text_domain' ),
-	  'parent_item'                => __( 'Parent Department', 'text_domain' ),
-	  'parent_item_colon'          => __( 'Parent Department:', 'text_domain' ),
-	  'new_item_name'              => __( 'New Department Name', 'text_domain' ),
-	  'add_new_item'               => __( 'Add Department', 'text_domain' ),
-	  'edit_item'                  => __( 'Edit Department', 'text_domain' ),
-	  'update_item'                => __( 'Update Department', 'text_domain' ),
-	  'view_item'                  => __( 'View Department', 'text_domain' ),
-	  'separate_items_with_commas' => __( 'Separate department with commas', 'text_domain' ),
-	  'add_or_remove_items'        => __( 'Add or remove departments', 'text_domain' ),
+	  'name'                       => _x( 'Memberships', 'Memberships Name', 'text_domain' ),
+	  'singular_name'              => _x( 'Membership', 'Membership Name', 'text_domain' ),
+	  'menu_name'                  => __( 'Memberships', 'text_domain' ),
+	  'all_items'                  => __( 'All Memberships', 'text_domain' ),
+	  'parent_item'                => __( 'Parent Membership', 'text_domain' ),
+	  'parent_item_colon'          => __( 'Parent Membership:', 'text_domain' ),
+	  'new_item_name'              => __( 'New Membership Name', 'text_domain' ),
+	  'add_new_item'               => __( 'Add Membership', 'text_domain' ),
+	  'edit_item'                  => __( 'Edit Membership', 'text_domain' ),
+	  'update_item'                => __( 'Update Membership', 'text_domain' ),
+	  'view_item'                  => __( 'View Membership', 'text_domain' ),
+	  'separate_items_with_commas' => __( 'Separate Membership with commas', 'text_domain' ),
+	  'add_or_remove_items'        => __( 'Add or remove Memberships', 'text_domain' ),
 	  'choose_from_most_used'      => __( 'Choose from the most used', 'text_domain' ),
-	  'popular_items'              => __( 'Popular Departments', 'text_domain' ),
-	  'search_items'               => __( 'Search Departments', 'text_domain' ),
+	  'popular_items'              => __( 'Popular Memberships', 'text_domain' ),
+	  'search_items'               => __( 'Search Memberships', 'text_domain' ),
 	  'not_found'                  => __( 'Not Found', 'text_domain' ),
-	  'no_terms'                   => __( 'No departments', 'text_domain' ),
-	  'items_list'                 => __( 'Departments list', 'text_domain' ),
-	  'items_list_navigation'      => __( 'Departments list navigation', 'text_domain' ),
+	  'no_terms'                   => __( 'No Memberships', 'text_domain' ),
+	  'items_list'                 => __( 'Memberships list', 'text_domain' ),
+	  'items_list_navigation'      => __( 'Memberships list navigation', 'text_domain' ),
 	);
 	$args = array(
 	  'labels'                     => $labels,
@@ -189,51 +262,6 @@ function cb_manage_departments_column( $display, $column, $term_id ) {
   }
   add_filter( 'manage_departments_custom_column', 'cb_manage_departments_column', 10, 3 );
 
-  /**
- * @param object $user The user object currently being edited.
- */
-function cb_edit_user_department_section( $user ) {
-	global $pagenow;
-  
-	$tax = get_taxonomy( 'departments' );
-  
-	/* Make sure the user can assign terms of the departments taxonomy before proceeding. */
-	if ( !current_user_can( $tax->cap->assign_terms ) )
-	  return;
-  
-	/* Get the terms of the 'departments' taxonomy. */
-	$terms = get_terms( 'departments', array( 'hide_empty' => false ) ); ?>
-  
-	<h3><?php _e( 'Departments' ); ?></h3>
-  
-	<table class="form-table">
-  
-	  <tr>
-		<th><label for="departments"><?php _e( 'Allocated Departments' ); ?></label></th>
-  
-		<td><?php
-  
-		/* If there are any departments terms, loop through them and display checkboxes. */
-		if ( !empty( $terms ) ) {
-			echo cb_custom_form_field('departments', $terms, $user->ID);
-		}
-  
-		/* If there are no departments terms, display a message. */
-		else {
-		  _e( 'There are no departments available.' );
-		}
-  
-		?></td>
-	  </tr>
-  
-	</table>
-  <?php }
-  
-  add_action( 'show_user_profile', 'cb_edit_user_department_section' );
-  add_action( 'edit_user_profile', 'cb_edit_user_department_section' );
-  add_action( 'user_new_form', 'cb_edit_user_department_section' );
-
-
 
 
 
@@ -254,7 +282,7 @@ function cb_edit_user_department_section( $user ) {
  		'id'               => $prefix . 'edit', 
  		'title'            => esc_html__( 'Category Metabox', 'cmb2' ), // Doesn't output for term boxes 
  		'object_types'     => array( 'term' ), // Tells CMB2 to use term_meta vs post_meta 
- 		'taxonomies'       => array( 'departments', 'post_tag' ), // Tells CMB2 which taxonomies should have these fields 
+ 		'taxonomies'       => array( 'departments' ), // Tells CMB2 which taxonomies should have these fields 
  		// 'new_term_section' => true, // Will display in the "Add New Category" section 
  	) ); 
    
@@ -278,9 +306,6 @@ function cb_edit_user_department_section( $user ) {
 		 'type'    => 'pw_multiselect',
 		 'options' => $users_id_and_name
 	 ) ); 
-
-  
- 	
 
   
  } 
